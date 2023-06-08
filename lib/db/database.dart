6fa3ad40a4c5af125b81f9b4760360225dfa2d20
springeye <henjue@gmail.com>
2023-06-08 11:57:02 +0800

    update db path

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 import 'package:floor/floor.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:sqflite_common_ffi/sqflite_ffi.dart' as sqflite;
 
@@ -21,7 +22,9 @@ abstract class AppDatabase extends FloorDatabase {
   @factoryMethod
   @preResolve
   static Future<AppDatabase> from(@Named("encryptionKey") String key) async {
-    var builder = $FloorAppDatabase.databaseBuilder('app_database.db');
+    var path =
+        await getApplicationSupportDirectory().then((value) => value.path);
+    var builder = $FloorAppDatabase.databaseBuilder('$path/app_database.db');
     return await builder.addCallback(
       Callback(
         onConfigure: (db) async {
