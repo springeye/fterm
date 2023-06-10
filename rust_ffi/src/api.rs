f4dbcb07bb3b91769a1d29bf07247046521bbc05
henjue <henjue@gmail.com>
2023-06-10 16:30:25 +0800

    add russqlite

--- rust_ffi/src/api.rs
+++ rust_ffi/src/api.rs
@@ -1,3 +1,30 @@
+use rusqlite::{Connection, Result};
+//
+// #[derive(Debug)]
+// struct Person {
+//     id: i32,
+//     name: String,
+//     data: Option<Vec<u8>>,
+// }
+fn db_version() {
+    let conn = Connection::open_in_memory().unwrap();
+     let version:Result<String> =conn.query_row("PRAGMA cipher_version",[], |row| row.get(0));
+    let is_cipher = match version {
+        Ok(_) => { true }
+        Err(_) => { false }
+    };
+    println!("支持加密========>{}", is_cipher);
+}
+
 pub fn get_version() -> String {
     String::from("Hello from Rust! 🦀")
+}
+#[cfg(test)]
+mod tests {
+    use crate::api::db_version;
+
+    #[test]
+    fn test_db_version() {
+        db_version();
+    }
 }
